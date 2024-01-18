7e0a0fbcad9bb9714bb0ed4956d845ad99e05627
yangwoohyeon <137463220+yangwoohyeon@users.noreply.github.com>
2024-01-18 20:07:36 +0900

    Add files via upload
     스프링부트 간단한 게시판 만들기 실습 코드

---
+++ src/main/java/hello/hellospring/aop/TimeTraceAop.java
@@ -0,0 +1,23 @@
+package hello.hellospring.aop;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+@Component
+@Aspect
+public class TimeTraceAop {
+    @Around("execution(* hello.hellospring..*(..))") //시간 측정을 실행할 범위 설정(이렇게 쓰면 전체 다 실행됨)
+
+    public Object execute(ProceedingJoinPoint joinPoint) throws Throwable {
+        long start = System.currentTimeMillis();
+        System.out.println("START: " + joinPoint.toString());
+        try {
+            return joinPoint.proceed();
+        } finally {
+            long finish = System.currentTimeMillis();
+            long timeMs = finish - start;
+            System.out.println("END: " + joinPoint.toString()+ " " + timeMs +
+                    "ms");
+        }
+    }
+}
